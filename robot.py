701552690af62400b9c4177b6170142418c2db95
Валерия Бунина <am-nyam@Air-Valeria-2.Dlink>
2018-04-01 11:32:57 +0300

    update9

--- robot.py
+++ robot.py
@@ -36,13 +36,44 @@ class BotHandler:
 
 ## TOKEN - INSERT YOUR TOKEN HERE
 token = "500356225:AAEzQwaTne7NgT1sxpJThEm7YrER7aGtxxg"
+greet_bot = BotHandler(token)
+greetings = ('здравствуй', 'привет', 'ку', 'здорово')
+now = datetime.datetime.now()
 
+def main():
+    new_offset = None
+    today = now.day
+    hour = now.hour
 
-@bot.message_handler(commands=['start'])
-def start(m):
-    msg = bot.send_message(m.chat.id, 'Как тебя зовут?')
+    while True:
+        greet_bot.get_updates(new_offset)
 
+        last_update = greet_bot.get_last_update()
+        if not last_update:
+            continue
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
 
-bot.polling()
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+##            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+##            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+##            today += 1
+
+        new_offset = last_update_id + 1
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
 
 
